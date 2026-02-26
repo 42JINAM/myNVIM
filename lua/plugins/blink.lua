a726838a08c5740502c507ef69e2740e52923e78
42JINAM <jinam@student.42.fr>
2026-02-26 20:02:35 +0100

    for c final

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,9 +1,19 @@
-return {
-  "saghen/blink.cmp",
-  opts = function(_, opts)
-    opts.keymap = opts.keymap or {}
-    -- 기존 preset은 그대로 두고, Tab 관련만 꺼 주기
-    opts.keymap["<Tab>"] = false      -- Tab으로 completion 이동/선택 안 함
-    opts.keymap["<S-Tab>"] = false    -- Shift+Tab도 비활성화
-  end,
+-- return {
+--   "saghen/blink.cmp",
+--   opts = function(_, opts)
+--     opts.keymap = opts.keymap or {}
+--     -- 기존 preset은 그대로 두고, Tab 관련만 꺼 주기
+--     opts.keymap["<Tab>"] = false      -- Tab으로 completion 이동/선택 안 함
+--     opts.keymap["<S-Tab>"] = false    -- Shift+Tab도 비활성화
+--   end,
+-- }
+--
+
+return { 
+  "Saghen/blink.cmp",  -- 대문자 S!
+  opts = {
+    keymap = {
+      ["<Tab>"] = { "snippet_forward", "fallback" },
+    },
+  },
 }
